cb85c26d4b4eb10ef6a5a2e912e523eef611ea52
IainJackson90 <>
2023-11-11 15:18:47 +0000

    Add: Basick random_range_number(x, y) function and main() function

--- run.py
+++ run.py
@@ -2,4 +2,38 @@ import math
 import random
 import operator
 
-def Main()
+
+def random_range_number(x, y):
+    """
+    Accepts a number range input between two numbers from the uses and
+    randomize them
+    """
+    while True:
+        try:
+            if y < x:
+                print("You need a number larger than your first value")
+                print("pleas try again\n")
+            else:
+                value_x = x
+                value_y = y
+                print("Random x:")
+                random_x = print(f"{random.randint(value_x, value_y)}")
+                print("Random y:")
+                random_y = print(f"{random.randint(value_x, value_y)}")
+                return random_x, random_y
+        except ValueError as e:
+            print(f"invalid data: {e}, pleas try again")
+            return False
+    return True
+
+
+def main():
+    """
+    This is the main function of that will run all the functions
+    """
+    user_x = int(input("Range start value:"))
+    user_y = int(input("Range end value:"))
+    random_range_number(user_x, user_y)
+
+
+main()
