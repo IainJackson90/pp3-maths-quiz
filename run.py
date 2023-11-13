68003cb966c7ecc280164bb435e5773363ac2688
IainJackson90 <>
2023-11-13 12:59:16 +0000

    Add: Logo to top of the main function

--- run.py
+++ run.py
@@ -5,27 +5,27 @@ import operator
 
 def user_input():
     """
-    Accepts a usser input and converts it to a integer values
-    If the value is not numerical value ot integer it will
+    Accepts a user input and converts it to a integer values
+    If the value is not numerical value or not integer it will
     display a value error
     """
     try:
         user_x = int(input("Range start value:"))
         user_y = int(input("Range end value:"))
-        number_of_rounds = int(input
-                               ("How many rounds would you like to play:"))
+        print("How many rounds would you like to play:")
+        n_rounds = int(input())
         print("\n")
     except ValueError:
         print("Invalid input. Please enter a valid integer.")
         not_integer = user_input()
         return not_integer
-    return user_x, user_y, number_of_rounds
+    return user_x, user_y, n_rounds
 
 
-def data_validation(user_value_x, user_value_y):
+def data_validation(user_value_x, user_value_y, rounds_playing):
     """
     Validates the input from the user and making sure that the first
-    value is not bigger thatn the second value
+    value is not bigger than the second value
     """
     while True:
         try:
@@ -37,7 +37,8 @@ def data_validation(user_value_x, user_value_y):
                 y_biger_than_x = user_input()
                 return y_biger_than_x
             else:
-                return x, y
+                number_of_rounds = rounds_playing
+                return x, y, number_of_rounds
         except ValueError as e:
             print(f"invalid data: {e}, pleas try again")
     return True
@@ -56,7 +57,7 @@ def random_range_number(x, y):
 def random_opperator(x):
     """
     Creates a random operator and if the value of x
-    is zero it will remove devide from the random choice
+    is zero it will remove divide from the random choice
     """
     op = {"/": operator.truediv,
           "*": operator.mul,
@@ -78,7 +79,7 @@ def random_opperator(x):
 def math_question(x, y, rand_op_str, op_func):
     """
     Creates a Math equation and accepts a user input that
-    evaluates wheter the user answer is right or wrong
+    evaluates whether the user answer is right or wrong
     """
     while True:
         try:
@@ -90,7 +91,7 @@ def math_question(x, y, rand_op_str, op_func):
                 print("Corect!")
                 break
             else:
-                print("Incorect!")
+                print("Incorrect!")
                 break
         except ValueError:
             print("Invalid input. Please enter a valid value.")
@@ -104,10 +105,27 @@ def main():
     This is the main function of that will run all the functions
     Keeps score of the quizz
     """
-    user_input_x, user_input_y, number_of_rounds = user_input()
-    x_range, y_range, n_rounds = data_validation(user_input_x, user_input_y)
+    print(r"""
+      __  __           _     _       
+     |  \/  |         | |   | |      
+     | \  / |   __ _  | |_  | |__    
+     | |\/| |  / _` | | __| | '_ \   
+     | |  | | | (_| | | |_  | | | |  
+     |_|  |_|  \__,_|  \__| |_| |_|  
+                                                             
+       ____            _           _ 
+      / __ \          (_)         | |
+     | |  | |  _   _   _   ____   | |
+     | |  | | | | | | | | |_  /   | |
+     | |__| | | |_| | | |  / /    |_|
+      \___\_\  \__,_| |_| /___|   (_)
+    """)
+    print("\n")
+    user_input_x, user_input_y, rounds = user_input()
+    x_range, y_range, n_of_rounds = data_validation(user_input_x,
+                                                    user_input_y, rounds)
     main_score = 0
-    for i in range(number_of_rounds):
+    for i in range(n_of_rounds):
         random_numb_x, random_numb_y = random_range_number(x_range, y_range)
         valuhate_x = int(random_numb_x)
         valuhate_y = int(random_numb_y)
@@ -116,10 +134,10 @@ def main():
             valuhate_x, valuhate_y, rand_op_str, op_func)
         if c_ans == u_ans:
             main_score += 1
-            print(f"Your score is: {main_score} out of {number_of_rounds}")
+            print(f"Your score is: {main_score} out of {n_of_rounds}")
             print("\n")
         else:
-            print(f"Your score is: {main_score}  out of {number_of_rounds} ")
+            print(f"Your score is: {main_score}  out of {n_of_rounds} ")
             print("\n")
 
 
